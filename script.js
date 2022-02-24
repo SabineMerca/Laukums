5b17556e02c1ee8e537face801635a1e66fada81
SabineMerca <sabinemerca@inbox.lv>
2022-02-24 14:52:37 +0200

    trijsturis

--- script.js
+++ script.js
@@ -12,3 +12,32 @@ function nolasa(m1,m2,m3){
         return false;
     }
 }
+function perimetrs(m1,m2,m3){
+    const m=nolasa();
+    m1=m.m1;m2=m.m2;m3=m.m3;
+    console.log(m1,m2,m3);
+    const p=m1+m2+m3;
+    console.log(p);
+    return(p);
+}
+function pusperimetrs(m1,m2,m3){
+    const pusperimetrs = perimetrs(m1,m2,m3)/2;
+    console.log(pusperimetrs);
+    return(pusperimetrs);
+}
+function laukums(m1,m2,m3){
+    const pusperimetrs = perimetrs(m1,m2,m3)/2;
+    let zlaukums=Math.sqrt(pusperimetrs*((pusperimetrs-1)*(pusperimetrs-m2)*(pusperimetrs-m3)));
+    console.log(zlaukums);
+    return zlaukums;
+}
+function irTrijsturis(m1,m2,m3){
+    if(m1<m2+m3&&m2<m1+m3&&m3<m1+m2){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+
